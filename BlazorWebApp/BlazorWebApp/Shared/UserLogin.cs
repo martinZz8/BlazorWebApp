9f85ef9a15ab621ecebfaac0867b7231ddb18095
martinZz8 <ewing552587@wp.pl>
2021-07-10 19:40:56 +0200

    Rozpoczęcie formularza logowania i rejestracji

---
+++ BlazorWebApp/BlazorWebApp/Shared/UserLogin.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlazorWebApp.Shared
+{
+    public class UserLogin
+    {
+        [Required(ErrorMessage = "Please eneter the user name")]
+        public string Username { get; set; }
+        [Required(ErrorMessage = "Please eneter password")]
+        public string Password { get; set; }
+    }
+}
